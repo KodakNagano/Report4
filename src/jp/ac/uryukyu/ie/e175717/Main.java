4c9b7294ea7080f76ca57ba5953f438c7ddb3d5f
Kodai Nagado <e175717@ie.u-ryukyu.ac.jp>
2018-01-18 12:26:31 +0900

    report4 step1

---
+++ src/jp/ac/uryukyu/ie/e175717/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e175717;
+
+public class Main {
+    public static void main (String args[]){
+        String  a;
+        a = null;
+        System.out.println(a.length());
+    }
+}
